89cd59f52e0029a4838fd815b875354241a859bc
Yeison Tapia <yftapial@ut.edu.co>
2019-06-17 18:24:34 -0500

    + Update components checkout [billingAddress, ShippingAddress]

--- _components/frontend/checkout/billingAddress.vue
+++ _components/frontend/checkout/billingAddress.vue
@@ -3,104 +3,148 @@
     <div class="row shadow-1">
       <div class="col-md-12 flex flex-center">
         <h1 class="q-headline text-primary ">
-          <q-chip color="primary">2</q-chip>
-          Billing Address
+          <q-chip color="primary">3</q-chip>
+          {{title}}
         </h1>
       </div>
       <div class="col-md-12 q-px-md">
-        
-        <div class="row gutter-x-sm">
-          <div class="col md-6">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input  v-model="checkoutData.attributes.paymentFirstName" float-label="First Name"/>
-            </q-field>
-          </div>
-          <div class="col md-6">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="checkoutData.attributes.paymentLastName" float-label="Last Name"/>
-            </q-field>
-          </div>
-        </div>
   
         <div class="row gutter-x-sm">
-          <div class="col md-12">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="checkoutData.attributes.paymentCompany" float-label=" Company name"/>
-            </q-field>
+          <div class="col md-6 q-pb-lg">
+            <q-checkbox v-model="shippingBillingIsSame" label="My shipping and billing addresses are the same." />
           </div>
         </div>
         
-        <div class="row gutter-x-sm">
-          <div class="col md-12">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="checkoutData.attributes.paymentAddress1" float-label=" Address 1"/>
-            </q-field>
-          </div>
-        </div>
-        
-        <div class="row gutter-x-sm">
-          <div class="col md-12">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="checkoutData.attributes.paymentAddress2" float-label=" Address 2"/>
-            </q-field>
-          </div>
-        </div>
-        
-        <div class="row gutter-x-sm">
-          <div class="col md-6">
-          <q-field
-            :error="false"
-            error-label="We need a valid email">
-            <q-input v-model="checkoutData.attributes.paymentCity" float-label="City"/>
-          </q-field>
-          </div>
+        <div v-if="!shippingBillingIsSame">
+          
+          <div class="row gutter-x-sm">
             <div class="col md-6">
-          <q-field
-            :error="false"
-            error-label="We need a valid email">
-            <q-input v-model="checkoutData.attributes.paymentZipCode" float-label="Zip/Postal code"/>
-          </q-field>
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-input
+                  v-model="checkoutData.attributes.paymentFirstName"
+                  float-label="First Name"/>
+              </q-field>
             </div>
-        </div>
-        
-        <div class="row gutter-x-sm">
-          <div class="col md-12">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="checkoutData.attributes.paymentCountry" float-label="Country"/>
-            </q-field>
+            <div class="col md-6">
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-input
+                  v-model="checkoutData.attributes.paymentLastName"
+                  float-label="Last Name"/>
+              </q-field>
+            </div>
           </div>
+          
+          <div class="row gutter-x-sm">
+            <div class="col md-12">
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-input
+                  v-model="checkoutData.attributes.paymentCompany"
+                  float-label=" Company name"/>
+              </q-field>
+            </div>
+          </div>
+  
+          <div class="row gutter-x-sm">
+            <div class="col md-12">
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-input
+                  v-model="checkoutData.attributes.paymentAddress1"
+                  float-label=" Address 1"/>
+              </q-field>
+            </div>
+          </div>
+  
+          <div class="row gutter-x-sm">
+            <div class="col md-12">
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-input
+                  v-model="checkoutData.attributes.paymentAddress2"
+                  float-label=" Address 2"/>
+              </q-field>
+            </div>
+          </div>
+  
+          <div class="row gutter-x-sm">
+            <div class="col md-12">
+              <q-field
+                :error="false"
+                error-label="This field is required">
+                <q-select
+                  @input="handleOnChangeCountry()"
+                  filter
+                  float-label="Country"
+                  v-model="checkoutData.attributes.paymentCountry"
+                  :options="$store.state.qlocations.countries"/>
+              </q-field>
+            </div>
+          </div>
+  
+          <div class="row gutter-x-sm">
+            <div class="col md-12">
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-select
+                  @input="handleOnChangeProvice()"
+                  filter
+                  float-label="State/Province"
+                  v-model="checkoutData.attributes.paymentZone"
+                  :options="provinces"/>
+              </q-field>
+            </div>
+          </div>
+          
+          <div class="row gutter-x-sm">
+            <div class="col md-6">
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-select
+                  filter
+                  float-label="City"
+                  v-model="checkoutData.attributes.paymentCity"
+                  :options="cities"/>
+              </q-field>
+            </div>
+            <div class="col md-6">
+              <q-field
+                :error="false"
+                error-label="We need a valid email">
+                <q-input
+                  v-model="checkoutData.attributes.paymentZipCode"
+                  float-label="Zip/Postal code"/>
+              </q-field>
+            </div>
+          </div>
+  
         </div>
   
-        <div class="row gutter-x-sm">
-          <div class="col md-12">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="checkoutData.attributes.paymentZone" float-label="State/Province"/>
-            </q-field>
-          </div>
-        </div>
-        
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  // COMPONENTS
+  import Treeselect from '@riophae/vue-treeselect'
+  import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+  
   export default {
     props:{
+      title:{
+        type: String,
+        default:'Billing Address'
+      },
       checkoutData:{
         type:Object,
         default:()=>({
@@ -108,9 +152,9 @@
             storeId: 0,
             customerId: 0,
             paymentMethod: '',
-            paymentMethod_id: 0,
+            paymentMethodId: 0,
             shippingMethod: '',
-            shippingMethod_id: 0,
+            shippingMethodId: 0,
             cartId: 0,
             paymentFirstName: '',
             paymentLastName: '',
@@ -135,13 +179,51 @@
         })
       }
     },
+    components:{
+      Treeselect
+    },
     data(){
       return{
-
+        shippingBillingIsSame: true,
+        provinces:[],
+        cities:[],
+      }
+    },
+    methods:{
+      handleOnChangeCountry(){
+        let params = {
+          refresh:true,
+          params:{
+            filter:{
+              country:this.checkoutData.attributes.paymentCountry
+            }
+          }
+        }
+        this.$store.dispatch('qlocations/GET_PROVINCES',params)
+        .then(response=>{
+          this.provinces = this.$store.state.qlocations.provinces
+        })
+        .catch(error=>{
+          this.$helper.alert.error(`Error ${error}`)
+        })
+      },
+      handleOnChangeProvice(){
+        let params = {
+          refresh:true,
+          params:{
+            filter:{
+              province_id:this.checkoutData.attributes.paymentZone
+            }
+          }
+        }
+        this.$store.dispatch('qlocations/GET_CITIES',params)
+        .then(response=>{
+          this.cities = this.$store.state.qlocations.cities
+        })
+        .catch(error=>{
+          this.$helper.alert.error(`Error ${error}`)
+        })
       }
     }
   }
 </script>
-
-<style scoped>
-</style>
--- _components/frontend/checkout/shippingAddress.vue
+++ _components/frontend/checkout/shippingAddress.vue
@@ -4,31 +4,28 @@
       <div class="col-md-12 flex flex-center">
         <h1 class="q-headline text-primary ">
           <q-chip color="primary">2</q-chip>
-          Shipping Address
+          {{title}}
         </h1>
       </div>
       <div class="col-md-12 q-px-md">
-  
-        <div class="row gutter-x-sm">
-          <div class="col md-12">
-            ASD
-          </div>
-        </div>
-        
         
         <div class="row gutter-x-sm">
           <div class="col md-6">
             <q-field
               :error="false"
               error-label="We need a valid email">
-              <q-input  v-model="formData.firstName" float-label="First Name"/>
+              <q-input
+                v-model="checkoutData.attributes.shippingFirstName"
+                float-label="First Name"/>
             </q-field>
           </div>
           <div class="col md-6">
             <q-field
               :error="false"
               error-label="We need a valid email">
-              <q-input v-model="formData.lastName" float-label="Last Name"/>
+              <q-input
+                v-model="checkoutData.attributes.shippingLastName"
+                float-label="Last Name"/>
             </q-field>
           </div>
         </div>
@@ -38,7 +35,9 @@
             <q-field
               :error="false"
               error-label="We need a valid email">
-              <q-input v-model="formData.companyName" float-label=" Company name"/>
+              <q-input
+                v-model="checkoutData.attributes.shippingCompany"
+                float-label=" Company name"/>
             </q-field>
           </div>
         </div>
@@ -48,7 +47,9 @@
             <q-field
               :error="false"
               error-label="We need a valid email">
-              <q-input v-model="formData.address1" float-label=" Address 1"/>
+              <q-input
+                v-model="checkoutData.attributes.shippingAddress1"
+                float-label=" Address 1"/>
             </q-field>
           </div>
         </div>
@@ -58,7 +59,39 @@
             <q-field
               :error="false"
               error-label="We need a valid email">
-              <q-input v-model="formData.address2" float-label=" Address 2"/>
+              <q-input
+                v-model="checkoutData.attributes.shippingAddress2"
+                float-label=" Address 2"/>
+            </q-field>
+          </div>
+        </div>
+        
+        <div class="row gutter-x-sm">
+          <div class="col md-12">
+            <q-field
+              :error="false"
+              error-label="This field is required">
+              <q-select
+                @input="handleOnChangeCountry()"
+                filter
+                float-label="Country"
+                v-model="checkoutData.attributes.shippingCountry"
+                :options="$store.state.qlocations.countries"/>
+            </q-field>
+          </div>
+        </div>
+        
+        <div class="row gutter-x-sm">
+          <div class="col md-12">
+            <q-field
+              :error="false"
+              error-label="We need a valid email">
+              <q-select
+                @input="handleOnChangeProvice()"
+                filter
+                float-label="State/Province"
+                v-model="checkoutData.attributes.shippingZone"
+                :options="provinces"/>
             </q-field>
           </div>
         </div>
@@ -68,34 +101,20 @@
             <q-field
               :error="false"
               error-label="We need a valid email">
-              <q-input v-model="formData.city" float-label="City"/>
+              <q-select
+                filter
+                float-label="City"
+                v-model="checkoutData.attributes.shippingCity"
+                :options="cities"/>
             </q-field>
           </div>
           <div class="col md-6">
             <q-field
               :error="false"
               error-label="We need a valid email">
-              <q-input v-model="formData.zipCode" float-label="Zip/Postal code"/>
-            </q-field>
-          </div>
-        </div>
-        
-        <div class="row gutter-x-sm">
-          <div class="col md-12">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="formData.country" float-label="Country"/>
-            </q-field>
-          </div>
-        </div>
-        
-        <div class="row gutter-x-sm">
-          <div class="col md-12">
-            <q-field
-              :error="false"
-              error-label="We need a valid email">
-              <q-input v-model="formData.province" float-label="State/Province"/>
+              <q-input
+                v-model="checkoutData.attributes.shippingZipCode"
+                float-label="Zip/Postal code"/>
             </q-field>
           </div>
         </div>
@@ -106,24 +125,120 @@
 </template>
 
 <script>
+  // COMPONENTS
+  import Treeselect from '@riophae/vue-treeselect'
+  import '@riophae/vue-treeselect/dist/vue-treeselect.css'
+
   export default {
+    props:{
+      title:{
+        type: String,
+        default:'Shipping Address'
+      },
+      checkoutData:{
+        type:Object,
+        default:()=>({
+          attributes: {
+            storeId: 0,
+            customerId: 0,
+            paymentMethod: '',
+            paymentMethodId: 0,
+            shippingMethod: '',
+            shippingMethodId: 0,
+            cartId: 0,
+            paymentFirstName: '',
+            paymentLastName: '',
+            paymentCompany: '',
+            paymentNit: '',
+            paymentAddress1: '',
+            paymentAddress2: '',
+            paymentCity: '',
+            paymentZipCode: '',
+            paymentCountry: '',
+            paymentZone: '',
+            shippingFirstName: '',
+            shippingLastName: '',
+            shippingCompany: '',
+            shippingAddress1: '',
+            shippingAddress2: '',
+            shippingCity: '',
+            shippingZipCode: '',
+            shippingCountry: '',
+            shippingZone: ''
+          }
+        })
+      }
+    },
+    watch:{
+      'checkoutData.attributes.shippingCountry':function (val, oldVal) {
+        let params = {
+          params:this.checkoutData.attributes
+        }
+        this.$store.dispatch('eCommerce/GET_SHIPPING_METHODS', params);
+      },
+      'checkoutData.attributes.shippingZone':function (val, oldVal) {
+        let params = {
+          params:this.checkoutData.attributes
+        }
+        this.$store.dispatch('eCommerce/GET_SHIPPING_METHODS',params);
+      },
+      'checkoutData.attributes.shippingCity':function (val, oldVal) {
+        let params = {
+          params:this.checkoutData.attributes
+        }
+        this.$store.dispatch('eCommerce/GET_SHIPPING_METHODS',params);
+      },
+      'checkoutData.attributes.shippingZipCode':function (val, oldVal) {
+        let params = {
+          params:this.checkoutData.attributes
+        }
+        this.$store.dispatch('eCommerce/GET_SHIPPING_METHODS', params);
+      }
+    },
+    components:{
+      Treeselect
+    },
     data(){
       return{
-        formData:{
-          firstName:'',
-          lastName:'',
-          companyName:'',
-          address1:'',
-          address2:'',
-          city:'',
-          zipCode:'',
-          country:'',
-          province:'',
+        provinces:[],
+        cities:[],
+      }
+    },
+    methods:{
+      handleOnChangeCountry(){
+        let params = {
+          refresh:true,
+          params:{
+            filter:{
+              country:this.checkoutData.attributes.shippingCountry
+            }
+          }
         }
+        this.$store.dispatch('qlocations/GET_PROVINCES',params)
+        .then(response=>{
+          this.provinces = this.$store.state.qlocations.provinces
+        })
+        .catch(error=>{
+          this.$helper.alert.error(`Error ${error}`)
+        })
+      },
+      handleOnChangeProvice(){
+        let params = {
+          refresh:true,
+          params:{
+            filter:{
+              province_id:this.checkoutData.attributes.shippingZone
+            }
+          }
+        }
+        this.$store.dispatch('qlocations/GET_CITIES',params)
+        .then(response=>{
+          this.cities = this.$store.state.qlocations.cities
+        })
+        .catch(error=>{
+          this.$helper.alert.error(`Error ${error}`)
+        })
       }
     }
   }
 </script>
-
-<style scoped>
-</style>
