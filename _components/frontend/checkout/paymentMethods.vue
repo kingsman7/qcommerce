766916f370b80d1c328f1367229ab5f8c5e93f84
Yeison Tapia <yftapial@ut.edu.co>
2019-06-19 16:26:44 -0500

    + Update payment methods Component, add binding vars

--- _components/frontend/checkout/paymentMethods.vue
+++ _components/frontend/checkout/paymentMethods.vue
@@ -7,15 +7,31 @@
           {{title}}
         </h1>
       </div>
-      <div class="col-md-12 q-my-sm q-mx-sm">
+      <div class="col-md-12 q-my-sm q-px-sm">
+        <q-item-separator />
         <q-field
-          :error="error.paymentMethodId.$error || false"
-          v-for="(paymentMethod, index) in $store.state.eCommerce.paymentMethods"
-          :key="index">
-          <q-radio
-            v-model="payment"
-            :val="paymentMethod"
-            :label="paymentMethod.title" />
+          :error="error.paymentMethod.$error"
+          error-label="Payment Method Required">
+          <q-item separator dense
+            tag="label"
+            v-for="(paymentMethod, index) in $store.state.eCommerce.paymentMethods"
+            :key="index">
+            <q-item-side>
+              <q-radio
+                style="display: block"
+                v-model="payment"
+                :val="paymentMethod"/>
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile label>
+                {{paymentMethod.title}}
+              </q-item-tile>
+              <q-item-tile sublabel>
+                {{paymentMethod.description}}
+              </q-item-tile>
+            </q-item-main>
+            <q-item-side :image="paymentMethod.mainImage.path" />
+          </q-item>
         </q-field>
       </div>
     </div>
@@ -67,6 +83,12 @@
         default:()=>{}
       }
     },
+    watch:{
+      payment(val){
+        this.checkoutData.attributes.paymentMethod = this.payment.name
+        this.checkoutData.attributes.paymentMethodId = this.payment.id
+      }
+    },
     data(){
       return{
         loading:false,
@@ -75,3 +97,6 @@
     },
   }
 </script>
+
+<style scoped>
+</style>
