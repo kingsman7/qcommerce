2c99738bab0f1fc2f38c0b2333fdf13be7c71a61
Yeison Tapia <yftapial@ut.edu.co>
2019-06-19 16:27:09 -0500

    + Update shipping methods Component

--- _components/frontend/checkout/shippingMethods.vue
+++ _components/frontend/checkout/shippingMethods.vue
@@ -7,15 +7,64 @@
           {{title}}
         </h1>
       </div>
-      <div class="col-md-12 q-my-sm q-mx-sm">
+      <div class="col-md-12 q-my-sm q-px-sm">
+        <q-item-separator />
         <q-field
-          v-for="(shippingMethod, index) in $store.state.eCommerce.shippingMethods"
-          :key="index">
-          <q-radio
-            v-model="shipping"
-            :val="shippingMethod"
-            :label="`${shippingMethod.title} ${shippingMethod.calculations.priceshow ? $n(shippingMethod.calculations.price) : ''}`" />
+          :error="error.shippingMethod.$error"
+          error-label="* Payment Method Required">
+          <q-item
+            separator dense
+            tag="label"
+            v-for="(shippingMethod, index) in $store.state.eCommerce.shippingMethods"
+            :key="index">
+            <q-item-side>
+              <q-radio
+                v-if="shippingMethod.calculations.price !== null && shippingMethod.calculations.price >= 0"
+                style="display: block"
+                v-model="shipping"
+                :val="shippingMethod"/>
+            </q-item-side>
+            <q-item-main>
+              <q-item-tile label>
+                {{shippingMethod.title}}
+              </q-item-tile>
+              <q-item-tile sublabel>
+                
+                <div  v-if="shippingMethod.calculations.priceshow || false">
+                  ${{$n(shippingMethod.calculations.price)}}
+                </div>
+  
+                <div v-if="shippingMethod.calculations.msj!=='error' && shippingMethod.calculations.msj!=='freeshipping'">
+                  <pre>{{shippingMethod.calculations.items}}</pre>
+  
+                  <div
+                    :key="index"
+                    v-for="(item, index) in shippingMethod.calculations.items">
+                    {{item}}
+                  </div>
+                  
+                </div>
+                
+                <div v-if="shippingMethod.calculations.status == 'error'">
+                  <span class="text-warning">
+                    {{shippingMethod.calculations.msj}}
+                  </span>
+                </div>
+                
+                <div v-if="shippingMethod.calculations.errors">
+                  <span class="text-warning">
+                    {{shippingMethod.calculations.errors}}
+                  </span>
+                </div>
+                
+              </q-item-tile>
+            </q-item-main>
+            <q-item-side
+              v-if="(shippingMethod.mainImage.path).indexOf('default.jpg') == -1 ? true : true"
+              :image="shippingMethod.mainImage.path" />
+          </q-item>
         </q-field>
+        
       </div>
     </div>
   </div>
@@ -77,3 +126,7 @@
     }
   }
 </script>
+
+<style scoped>
+
+</style>
