0683aa2efa372a96f5a2b41b931c68ac175a2adb
Yeison Tapia <yftapial@ut.edu.co>
2019-06-20 11:52:01 -0500

    + fix responsive Customer Information Component

--- _components/frontend/checkout/customerInformation.vue
+++ _components/frontend/checkout/customerInformation.vue
@@ -1,14 +1,43 @@
 <template>
   <div>
     <div class="row shadow-1">
-      <div class="col-md-12 flex flex-center">
+      <div class="col-xs-12 col-sm-12 flex flex-center">
         <h1 class="q-headline text-primary">
           <q-chip color="primary">1</q-chip>
           {{title}}
         </h1>
       </div>
-      <div class="col-md-12">
-        <userLoginRegister />
+      <div class="col-xs-12 col-md-12 q-px-sm">
+  
+        <q-item-separator />
+        
+        <div v-if="$store.state.auth.userData || false">
+          
+          <q-card class="no-shadow flex flex-center">
+            <q-card-media>
+              <img
+                :src="getImageUrl($store.state.auth.userData.mainImage)"
+                 alt="avatar"
+                 style="width: 200px;height: 200px; border-radius: 50%">
+            </q-card-media>
+            <q-card-title>
+              {{$store.state.auth.userData.fullName}}
+            </q-card-title>
+            <q-card-main>
+              <p>{{$store.state.auth.userData.email}}</p>
+            </q-card-main>
+            <q-card-separator />
+            <q-card-actions v-if="false">
+              <q-btn flat color="primary" icon="person" label="Profile" />
+              <q-btn flat color="primary" icon="exit_to_app" label="LogOut" />
+            </q-card-actions>
+          </q-card>
+          
+        </div>
+        <div v-else>
+          <userLoginRegister @logged="handleOnLoggedUser"/>
+        </div>
+        
       </div>
     </div>
   </div>
@@ -27,6 +56,22 @@
     },
     components:{
       userLoginRegister,
+    },
+    methods:{
+      handleOnLoggedUser(){
+        console.warn('logged')
+      },
+      getImageUrl(url){
+        let image;
+        if (!url) {
+          image = this.fields.mainImage.value;
+          if (this.fields.mainImage.value.indexOf('data:image') < 0)
+            image = config('apiRoutes.api.base_url') + '/' + this.fields.mainImage.value;
+        } else
+          image = config('apiRoutes.api.base_url') + '/' + url;
+
+        return image;
+      }
     }
   }
 </script>
