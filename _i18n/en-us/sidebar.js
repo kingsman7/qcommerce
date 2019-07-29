1a1a6963321a1175e58df661afd40590b55b108b
michael <38920570+msolanoimagina@users.noreply.github.com>
2019-07-29 12:35:26 -0500

    +Add: Translations (i18n)

---
+++ _i18n/en-us/sidebar.js
@@ -0,0 +1,11 @@
+export default {
+  adminGroup : 'Commerce',
+  adminProducts : 'Product List',
+  adminProductCreate : 'Product Create',
+  adminProductEdit : 'Product Edit',
+  adminCategories : 'Product Categories',
+  adminOptions : 'Product Options',
+  adminValues : 'Options Values',
+  adminPaymentMethods : 'Payment methods',
+  adminShippingMethods : 'Shipping methods'
+}
---
+++ _i18n/es/sidebar.js
@@ -0,0 +1,11 @@
+export default {
+  adminGroup : 'Tienda',
+  adminProducts : 'Productos',
+  adminProductCreate : 'Creación de Productos',
+  adminProductEdit : 'Editar Producto',
+  adminCategories : 'Categorias de Producto',
+  adminOptions : 'Opciones de Producto',
+  adminValues : 'Valores de Opción',
+  adminPaymentMethods : 'Métodos de pago',
+  adminShippingMethods : 'Métodos de envío'
+}
