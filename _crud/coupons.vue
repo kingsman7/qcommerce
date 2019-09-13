5ed1ff3819e231572fa6b36d3aaaf2e68df9128c
Yeison Tapia <yftapial@ut.edu.co>
2019-09-13 16:29:17 -0500

    + Update translations in headers table coupons

--- _crud/coupons.vue
+++ _crud/coupons.vue
@@ -13,11 +13,11 @@
           read: {
             columns: [
               {name: 'id', label: this.$tr('ui.form.id'), field: 'id', align: 'left'},
-              {name: 'code', label: 'code', field: 'code', align: 'left'},
-              {name: 'dateStart', label: 'dateStart', field: 'dateStart', align: 'left'},
-              {name: 'dateEnd', label: 'dateEnd', field: 'dateEnd', align: 'left'},
-              {name: 'quantityTotal', label: 'quantityTotal', field: 'quantityTotal', align: 'left'},
-              {name: 'quantityTotalCustomer', label: 'quantityTotalCustomer', field: 'quantityTotalCustomer', align: 'left'},
+              {name: 'code', label: this.$tr('qcommerce.layout.form.code'), field: 'code', align: 'left'},
+              {name: 'dateStart', label: this.$tr('qcommerce.layout.form.dateStart'), field: 'dateStart', align: 'left'},
+              {name: 'dateEnd', label: this.$tr('qcommerce.layout.form.dateEnd'), field: 'dateEnd', align: 'left'},
+              {name: 'quantityTotal', label: this.$tr('qcommerce.layout.form.quantityTotal'), field: 'quantityTotal', align: 'left'},
+              {name: 'quantityTotalCustomer', label: this.$tr('qcommerce.layout.form.quantityTotalCustomer'), field: 'quantityTotalCustomer', align: 'left'},
               {name: 'actions', label: this.$tr('ui.form.actions'), align: 'right'},
             ],
             requestParams: {},
