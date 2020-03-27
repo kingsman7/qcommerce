8d9eb72523d4f94d1f01873c01e416cae71a6b4f
YeisonTapia <yftapial@ut.edu.co>
2020-03-27 14:59:20 -0500

    Add menu item, for recursive items of menu

---
+++ _components/frontend/categories/menuItems.vue
@@ -0,0 +1,67 @@
+<template>
+	<q-list
+		:bordered="bordered"
+		class="bg-grey-2">
+		<div
+			v-for="(item, indexItem) in items"
+			:key="indexItem">
+			<q-item
+				@click="goToCategory(item)"
+				v-if="!item.children"
+				clickable
+				v-ripple>
+				<q-item-section>
+					{{item.title}}
+				</q-item-section>
+			</q-item>
+			<q-expansion-item
+				:content-inset-level="0.0"
+				expand-separator
+				:label="item.title"
+				v-else >
+				<menuItems
+					:bordered="false"
+					:items="item.children"/>
+			</q-expansion-item>
+		</div>
+	</q-list>
+</template>
+
+<script>
+	export default {
+		name: 'menuItems',
+	  props:{
+	    items:{
+				type: Array,
+				default: () => []
+			},
+			bordered:{
+	    	type: Boolean,
+				default: false
+			}
+		},
+		methods:{
+			goToCategory(category){
+				this.$router.push({
+					name: 'products.index.by.category',
+					params: {
+						slugCategory: category.slug
+					}
+				})
+				console.warn(category.slug)
+			}
+		}
+	}
+</script>
+
+<style>
+	.q-item{
+		border-bottom: #e1e1e1 1px solid;
+	}
+	.border-l{
+		border-left: 1px solid #e1e1e1;
+	}
+	.border-r{
+		border-right: 1px solid #e1e1e1
+	}
+</style>
