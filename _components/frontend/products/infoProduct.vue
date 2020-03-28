4a179c5f95c5fd2c3cedfaf4959ddebaf619d49f
YeisonTapia <yftapial@ut.edu.co>
2020-03-28 10:23:40 -0500

    Add component for show information product

---
+++ _components/frontend/products/infoProduct.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12">
+      <q-card class="my-card no-shadow card-product">
+        <q-img
+          class="cursor-pointer	"
+          @click="goToProduct(product)"
+          spinner-color="white"
+          :ratio="1"
+          transition="flip-left"
+          :src="product.mainImage.path"
+          basic >
+        </q-img>
+        <q-card-actions class="row">
+          <div class="col-md-12 flex flex-center text-subtitle1">
+            <q-btn dense flat rounded>
+              {{product.name}}
+            </q-btn>
+          </div>
+          <div class="col-md-12 flex flex-center text-subtitle1 text-bold text-primary">
+            $ {{$trc(product.price)}}
+          </div>
+        </q-card-actions>
+      </q-card>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:{
+      product:{
+        type: Object,
+        required: true,
+        default: () => {}
+      }
+    },
+    methods:{
+      goToProduct(product){
+        console.warn(product)
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
